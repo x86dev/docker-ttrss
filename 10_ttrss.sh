75fb76b4a2931ff2e19c9efa5c2ef85a96c1b5f6
Roman Golovin <danteek@gmail.com>
2014-07-03 01:26:50 +0300

    Better conditions handling

--- 10_ttrss.sh
+++ 10_ttrss.sh
@@ -6,25 +6,28 @@ set -x
 php /root/configure-db.php
 php /root/configure-plugin-mobilize.php
 
-#Backward compatibility
+# Generate the TLS certificate for our Tiny Tiny RSS server instance.
 if [ -f /etc/ssl/private/ttrss.key ] && [ -f /etc/ssl/certs/ttrss.cert ]
 then
-  ln -s /etc/ssl/private/ttrss.key /root/certs/key 
-  ln -s /etc/ssl/certs/ttrss.cert /root/certs/cert 
-fi
-
-# Generate the TLS certificate for our Tiny Tiny RSS server instance.
-if [ ! -f /root/certs/key ] && [ ! -f /root/certs/cert ]
-then
-  openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 \
-      -subj "/C=US/ST=World/L=World/O=ttrss/CN=ttrss" \
-      -keyout "/root/certs/key" \
-      -out "/root/certs/cert"
+  if [ ! -f /root/certs/key ] && [ ! -f /root/certs/cert ]
+  #Backward compatibility
+  then
+    ln -s /etc/ssl/private/ttrss.key /root/certs/key 
+    ln -s /etc/ssl/certs/ttrss.cert /root/certs/cert 
+  fi
+else
+  if [ ! -f /root/certs/key ] && [ ! -f /root/certs/cert ]
+  then
+    openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 \
+        -subj "/C=US/ST=World/L=World/O=ttrss/CN=ttrss" \
+        -keyout "/root/certs/key" \
+        -out "/root/certs/cert"
+  fi
 
   ln -s /root/certs/key /etc/ssl/private/ttrss.key
   ln -s /root/certs/cert /etc/ssl/certs/ttrss.cert
-
-  chmod 600 -R "/root/certs"
-  chmod 600 "/etc/ssl/private/ttrss.key"
-  chmod 600 "/etc/ssl/certs/ttrss.cert"
 fi
+
+chmod 600 -R "/root/certs"
+chmod 600 "/etc/ssl/private/ttrss.key"
+chmod 600 "/etc/ssl/certs/ttrss.cert"
