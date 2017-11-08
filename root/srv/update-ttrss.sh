5cf30a19dde9cc197f4e89fad9639ffd9de4b94d
x86dev <x86dev@users.noreply.github.com>
2017-11-08 22:25:02 +0100

    update-ttrss.sh: Removed News+ API plugin (deprecated / dead).

--- root/srv/update-ttrss.sh
+++ root/srv/update-ttrss.sh
@@ -22,16 +22,6 @@ update_plugin_mobilize()
     sed -i -e "s/<?$/<?php/g" ${TTRSS_PATH}/plugins/mobilize/m.php
 }
 
-# For use with News+ on Android. Buy the Pro version -- I love it!
-update_plugin_newsplus()
-{
-    echo "Updating: News+ plugin"
-    ( cd ${TTRSS_PATH}/plugins/api_newsplus && git pull origin HEAD )
-
-    # Link plugin to TTRSS.
-    ln -f -s ${TTRSS_PATH}/plugins/api_newsplus/api_newsplus/init.php ${TTRSS_PATH}/plugins/api_newsplus/init.php
-}
-
 update_plugin_feediron()
 {
     echo "Updating: FeedIron"
@@ -69,7 +59,6 @@ update_common()
 
 update_ttrss
 update_plugin_mobilize
-update_plugin_newsplus
 update_plugin_feediron
 update_theme_feedly
 update_common
