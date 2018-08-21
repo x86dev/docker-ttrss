f1f5fb1c36c2f84d49dcc414d29abba8f1a55836
Noah <9165575+noahvrz@users.noreply.github.com>
2018-08-21 17:09:40 -0400

    Added Fever API plugin.

--- root/srv/setup-ttrss.sh
+++ root/srv/setup-ttrss.sh
@@ -66,6 +66,7 @@ setup_ttrss()
         git clone --depth=1 https://github.com/sepich/tt-rss-mobilize.git ${TTRSS_PATH}/plugins/mobilize
         git clone --depth=1 https://github.com/m42e/ttrss_plugin-feediron.git ${TTRSS_PATH}/plugins/feediron
         git clone --depth=1 https://github.com/levito/tt-rss-feedly-theme.git ${TTRSS_PATH}/themes/feedly-git
+        git clone --depth=1 https://github.com/DigitalDJ/tinytinyrss-fever-plugin ${TTRSS_PATH}/themes/fever
     fi
 
     # Add initial config.
