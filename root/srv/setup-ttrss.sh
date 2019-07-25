472b083f7d01f8b45d3d86437d8282cb2c259241
x86dev <x86dev@users.noreply.github.com>
2019-07-25 23:04:33 +0200

    setup-ttrss.sh: Use the new feediron repo.

--- root/srv/setup-ttrss.sh
+++ root/srv/setup-ttrss.sh
@@ -69,7 +69,7 @@ setup_ttrss()
 
         mkdir -p ${TTRSS_PATH_PLUGINS}
         git clone --depth=1 https://github.com/sepich/tt-rss-mobilize.git ${TTRSS_PATH_PLUGINS}/mobilize
-        git clone --depth=1 https://github.com/m42e/ttrss_plugin-feediron.git ${TTRSS_PATH_PLUGINS}/feediron
+        git clone --depth=1 https://github.com/feediron/ttrss_plugin-feediron.git ${TTRSS_PATH_PLUGINS}/feediron
 
         mkdir -p ${TTRSS_PATH_THEMES}
         git clone --depth=1 https://github.com/levito/tt-rss-feedly-theme.git ${TTRSS_PATH_THEMES}/levito-feedly-git
